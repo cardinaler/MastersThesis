823e76b0f5b1b2fe7ae3f987916bbc39e3ec4a4a
cardinaler <ruslan05kasumov@mail.ru>
2026-09-01 00:13:16 +0500

    project structure added to README. pava.py is now a separate file

--- src/isotonic_grad_descent.py
+++ src/isotonic_grad_descent.py
@@ -1,28 +1,6 @@
 import numpy as np
+from src.pava import pava
 
-
-def pava(y):
-    """Проекция на конус монотонно невозрастающих функций."""
-    n = y.shape[0]
-    u = y.copy().astype(float)
-    blocks = [[i, 1, u[i]] for i in range(n)]
-
-    i = 0
-    while i < len(blocks) - 1:
-        if blocks[i][2] > blocks[i+1][2]:
-            start, n1, val1 = blocks[i]
-            _, n2, val2 = blocks[i+1]
-            new_val = (n1 * val1 + n2 * val2) / (n1 + n2)
-            blocks[i] = [start, n1 + n2, new_val]
-            blocks.pop(i + 1)
-            if i > 0:
-                i -= 1
-        else:
-            i += 1
-
-    for start, count, val in blocks:
-        u[start: start + count] = val
-    return u
 
 
 class isotonic_grad_descent:
